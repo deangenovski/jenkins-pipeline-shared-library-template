2059e9b2dc24ff488d7908ae75c5a5a54e287de0
Deyan Genovski <deangenovski@gmail.com>
2018-03-04 20:59:16 +0200

    test

--- vars/buildTide.groovy
+++ vars/buildTide.groovy
@@ -4,7 +4,11 @@ import com.tideaccount.android.jenkins.BuildType
 def call(List<BuildType> buildTypes, String keystore, String keystorePassword, String keyAlias, String keyPassword) {
     echo "hi"
 
-    echo(credentials('keyPassword'))
+    withCredentials([file(credentialsId: 'keystore', variable: 'KEYSTORE_LOCATION'),
+                     string(credentialsId: 'keystore-password', variable: 'KEYSTORE_PASSWORD'),
+                     string(credentialsId: 'key-password', variable: 'KEY_PASSWORD')]) {
+        echo("works")
+    }
 }
 
 
