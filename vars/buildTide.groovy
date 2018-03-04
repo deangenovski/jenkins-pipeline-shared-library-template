23471aa1097acc408198e3387f101f8a3eba981a
Deyan Genovski <deangenovski@gmail.com>
2018-03-04 20:54:01 +0200

    Experiment with credentials

--- vars/buildTide.groovy
+++ vars/buildTide.groovy
@@ -2,6 +2,9 @@
 import com.tideaccount.android.jenkins.BuildType
 
 def call(List<BuildType> buildTypes, String keystore, String keystorePassword, String keyAlias, String keyPassword) {
-    echo("hi")
+    echo "hi"
+
+    echo(credentials('keyPassword'))
 }
+
 
