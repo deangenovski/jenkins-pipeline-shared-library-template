cbdf4d3af5ea519a58a0ce74e34c0c1f6d67a0ea
Deyan Genovski <deangenovski@gmail.com>
2018-03-05 00:19:32 +0200

    ...

--- vars/assembleTide.groovy
+++ vars/assembleTide.groovy
@@ -1,5 +1,6 @@
 #!groovy
 import com.tideaccount.android.jenkins.BuildType
+import com.tideaccount.android.jenkins.Constants
 import com.tideaccount.android.jenkins.Keystore
 
 import static com.tideaccount.android.jenkins.Util.gradleOperation
@@ -9,12 +10,13 @@ def call(Integer buildNumberArg, String buildNameArg, BuildType... buildTypesArg
 
     withCredentials(this) { Keystore keystoreArg ->
         gradleOperation(this) {
+            buildStepFormat = Constants.ASSEMBLE_FORMAT
             buildTypes = buildTypesArg.toList()
             buildNumber = buildNumberArg
             buildName = buildNameArg
             keystore = keystoreArg
         }
-        
+
     }
 
 }
