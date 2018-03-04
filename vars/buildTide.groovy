5c6701d68be13ab4001e42ee693f5c174529a70a
Deyan Genovski <deangenovski@gmail.com>
2018-03-04 21:11:09 +0200

    Test

--- vars/buildTide.groovy
+++ vars/buildTide.groovy
@@ -4,11 +4,16 @@ import com.tideaccount.android.jenkins.BuildType
 def call(BuildType... buildTypes) {
     echo "hi"
 
+    def cloj = { String keystore ->
+        echo("hi from clojure")
+        echo(keystore)
+
+    }
+
     withCredentials([file(credentialsId: 'keystore', variable: 'KEYSTORE_LOCATION'),
                      string(credentialsId: 'keystore-password', variable: 'KEYSTORE_PASSWORD'),
                      string(credentialsId: 'key-password', variable: 'KEY_PASSWORD')]) {
-        sh("echo $KEYSTORE_LOCATION")
-        echo(KEYSTORE_LOCATION)
+       cloj(KEYSTORE_LOCATION)
     }
 }
 
