c0509f3713039975c5e948397ad667860a277357
Deyan Genovski <deangenovski@gmail.com>
2018-03-05 02:07:20 +0200

    ..

--- vars/killConcurrent.groovy
+++ vars/killConcurrent.groovy
@@ -7,13 +7,20 @@ def call() {
 
     def myBuild = currentBuild.rawBuild.getPreviousBuildInProgress()
 
+    echo(myBuild.toString())
+
     while (myBuild != null) {
 
         echo("Build found" + myBuild.toString())
-        echo(myBuild.getExecution().isComplete().toString())
+        def execution = myBuild.getExecution()
+        if (execution != null) {
+            echo(execution.isComplete().toString())
+        } else {
+            echo("null execution")
+        }
         echo(myBuild.getEnvironment(null).BUILD_NUMBER)
 
-        myBuild = myBuild.getPreviousBuild()
+        myBuild = myBuild.getPreviousBuildInProgress()
     }
 
 }
