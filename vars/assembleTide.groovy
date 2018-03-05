c5245fa56c56b19b334e58fce1c44db2f589d79e
Deyan Genovski <deangenovski@gmail.com>
2018-03-05 11:11:34 +0200

    Removed unused method arguments

--- vars/assembleTide.groovy
+++ vars/assembleTide.groovy
@@ -6,14 +6,12 @@ import com.tideaccount.android.jenkins.Keystore
 import static com.tideaccount.android.jenkins.Util.gradleOperation
 import static com.tideaccount.android.jenkins.Util.withCredentials
 
-def call(Integer buildNumberArg, String buildNameArg, BuildType... buildTypesArg) {
+def call(BuildType... buildTypesArg) {
 
     withCredentials(this) { Keystore keystoreArg ->
         gradleOperation(this) {
             buildStepFormat = Constants.ASSEMBLE_FORMAT
             buildTypes = buildTypesArg.toList()
-            buildNumber = buildNumberArg
-            buildName = buildNameArg
             keystore = keystoreArg
         }
 
