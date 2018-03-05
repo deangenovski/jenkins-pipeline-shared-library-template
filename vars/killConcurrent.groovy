fcd1cc0f5f32e064295185316a56f3a906bff84d
Deyan Genovski <deangenovski@gmail.com>
2018-03-05 02:24:42 +0200

    .

--- vars/killConcurrent.groovy
+++ vars/killConcurrent.groovy
@@ -10,10 +10,6 @@ def call() {
         if (build.getEnvironment(null).BRANCH_NAME == BRANCH_NAME) {
             echo("Killing")
             build.doKill()
-        } else {
-            echo("not killing")
-            echo(build.getEnvironment(null).toString())
-            echo(BRANCH_NAME)
         }
 
         build = build.getPreviousBuildInProgress()
